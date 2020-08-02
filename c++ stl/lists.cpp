577f471a76cf474f499d41707c761273f290e737
gunishmatta <gunishmatta@gmail.com>
2020-08-02 14:36:40 +0530

    Added lists in stl

---
+++ c++ stl/lists.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+
+//List uses all fuctions same as  vectors and lists are preffered when you want to do something at the fromt
+
+list<int> ls;
+ls.push_back(2); //{2}
+ls.push_back(5); //{2,5}
+ls.push_front(1); //{1,2,5}
+
+
+
+
+
+  return 0;
+}
