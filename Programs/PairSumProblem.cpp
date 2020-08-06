a98c44dc648199993f9509fb19728d1e896b188a
gunishmatta <gunishmatta@gmail.com>
2020-08-06 19:24:34 +0530

    Two pair problem

---
+++ Programs/PairSumProblem.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+//given a sorted array find the pair of elements that sum to k
+int main() {
+
+  int a[10] ={1,3,4,6,7,8};
+int key=14;
+int i =0;
+int j =(sizeof(a)/sizeof(int))-1;
+
+while(i<j)
+{
+int  currentSum = a[i]+a[j];
+  if (currentSum>key) {
+    j--;
+  }
+  else if (currentSum<key) {
+    i++;
+  }
+  else if (currentSum==key) {
+  std::cout <<a[i]<<" and "<<a[j]<< '\n';
+  }
+
+}
+
+  return 0;
+}
