4628f020e86ed4f2f23cc4dcaec7cf613d2c96a9
gunishmatta <gunishmatta@gmail.com>
2020-08-08 13:19:23 +0530

    wave printing in 2d arrays

---
+++ CBlocks/Arrays/2DArrays/waveprint.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int a[3][3]={{0,1,2},{2,3,4},{4,5,6}};
+int n = 3;
+for (int column = 0; column < 3; column++) {
+if (column%2==0) {
+  for (int row = 0; row < 3; row++) {
+std::cout <<a[row][column]<<" ";
+  }
+}
+
+  else
+  {
+  for (int row = 2; row >= 0; row--) {
+    std::cout <<a[row][column];
+  }
+}
+std::cout<< '\n';
+}
+}
+
+  return 0;
+}
