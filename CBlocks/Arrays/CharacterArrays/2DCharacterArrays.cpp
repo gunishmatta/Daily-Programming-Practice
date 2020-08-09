958d46164a6c8ebfe01fe165f7102bf33b77c858
gunishmatta <gunishmatta@gmail.com>
2020-08-09 13:27:56 +0530

    2d char array

---
+++ CBlocks/Arrays/CharacterArrays/2DCharacterArrays.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+char ch[10][10] = {{'a','b','c','d','\0'},{'e','f','g','h','\0'}};
+
+char a[10][10] = {"abc","def","efg"};
+
+std::cout <<ch[0]<< '\n';
+
+std::cout <<ch[1]<< '\n';
+
+for (int i = 0; i < 2; i++) {
+for (int j = 0; j < 5; j++) {
+std::cout <<ch[i][j];
+}
+std::cout<< '\n';
+}
+
+
+for (int i = 0; i < 2; i++) {
+for (int j = 0; j < 3; j++) {
+std::cout <<a[i][j];
+}
+std::cout<< '\n';
+}
+
+
+  return 0;
+}
