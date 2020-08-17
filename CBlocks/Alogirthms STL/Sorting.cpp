dd6d5726d04e770311005c6943e4d295eae79518
gunishmatta <gunishmatta@gmail.com>
2020-08-17 12:38:06 +0530

    sorting in stl

---
+++ CBlocks/Alogirthms STL/Sorting.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+bool compare(int a, int b)
+{
+  return a>b;
+}
+int main() {
+int n;
+cin>>n;
+int a[n];
+for (int i = 0; i < n; i++) {
+cin>>a[i];
+}
+sort(a,a+n,compare);
+for (int j = 0; j < n; j++) {
+std::cout <<a[j]<<" ";
+}
+
+
+  return 0;
+}
