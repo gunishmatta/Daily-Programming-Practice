8c6015d48ef2be2892653285ff8a986edbd0c11b
gunishmatta <gunishmatta@gmail.com>
2020-08-27 11:57:36 +0530

    clear bits from i to jth index

--- CBlocks/Bit Manipulation/ClearLast_i_Bits.cpp
+++ CBlocks/Bit Manipulation/ClearLast_i_Bits.cpp
@@ -4,18 +4,28 @@ using namespace std;
 
 void clear_lastn_bits(int &n,int i)
 {
-  int mask = (-1<<i);
+  int mask = (-1<<i+1);
    n =  n & mask;
 }
 
-
+void clear_itoj_bits(int &n,int i,int j)
+{
+  int first_mask = (~0)<<(j+1);
+  int second_mask = (1<<i)-1; //or we can use 2^n -1
+  int final_mask = first_mask|second_mask;
+n = n&final_mask;
+}
     int main() {
 
        int n;
        cin>>n;
 int i;
+int j;
 cin>>i;
-clear_lastn_bits(n,i);
+cin>>j;
+//clear_lastn_bits(n,i);
+clear_itoj_bits(n,i,j);
 cout<<n<<endl;
+
 
 }
