2f02193fdb55c126e350cb343ca624a050c12913
gunishmatta <gunishmatta@gmail.com>
2020-08-28 14:59:10 +0530

    Count bits optimised

--- CBlocks/Bit Manipulation/Count_Set_bits.cpp
+++ CBlocks/Bit Manipulation/Count_Set_bits.cpp
@@ -10,10 +10,20 @@ n = n>>1;
   }
   return ans;
 }
+//optimised method
+int countbitsfaster(int n)
+{
+  int ans =0;
+  while (n>0) {
+n = n&(n-1);
+ans++;
+  }
+return ans;
+}
 int main() {
   int n;
   cin>>n;
-  cout<<countbits(n)<<endl;
+  cout<<countbitsfaster(n)<<endl;
 
   return 0;
 }
