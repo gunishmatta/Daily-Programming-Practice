6e31daefc6e880c99931bf94f83eca8bd2184e89
gunishmatta <gunishmatta@gmail.com>
2020-08-31 13:27:30 +0530

    Count set bits from 1 to n

---
+++ CBlocks/Programs/CountSetBitsfrom_1ton.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main() {
+	// int Q;
+	// cin>>Q;
+	// while(Q>0)
+	// {
+int a;
+int b;
+int sum =0;
+cin>>a>>b;
+while(a<=b)
+{
+sum += __builtin_popcount(a);
+a++;
+}
+cout<<sum<<endl;
+
+
+
+	return 0;
+}
