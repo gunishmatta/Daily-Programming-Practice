e92d301685cbb8aba0f54ce1e8ebeb5303ae9b6c
gunishmatta <gunishmatta@gmail.com>
2020-09-06 18:39:26 +0530

    Product using recursion without using * operator

---
+++ CBlocks/Recursion/Product_using_Recursion.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int product(int a,int b)
+{
+if(b==0)
+{
+  return 0;
+}
+
+  if (b==1) {
+    return a;
+  }
+  return a+product(a,b-1);
+}
+int main() {
+  int a,b;
+  cin>>a>>b;
+  std::cout <<product(a,b)<< '\n';
+  return 0;
+}
