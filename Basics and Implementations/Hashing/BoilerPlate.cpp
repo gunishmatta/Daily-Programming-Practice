a7f1994f75d57a4cec59f5b5f8856d22507a5cef
Gunish Matta <gunishmatta@gmail.com>
2020-12-07 22:51:01 +0530

    Hashing print and insert functions

--- Basics and Implementations/Hashing/BoilerPlate.cpp
+++ Basics and Implementations/Hashing/BoilerPlate.cpp
@@ -9,8 +9,7 @@ public:
 	T value;
 	Node<T>* next;
 
-	Node(string key,val)
-	{
+	Node(string key,T val){
 		this->key=key;
 		value=val;
 		next = NULL;
@@ -25,13 +24,27 @@ public:
 	}
 	
 };
-
+template <typename T>
 class HashTable
 {
 int table_size;
 	int current_size;
 	Node<T> **table;
 	
+int hashFn(string key)
+{
+	int index=0;
+	int power=1;
+	for (int i = 0; i < key.length()-1; i++)
+	{
+		index = index+(key[i]*power)%table_size;
+		index = index%table_size;
+		power = (power*27)%table_size;
+
+	}
+	return index;
+}
+
 public:
 
 	HashTable(int ts=7)
@@ -47,7 +60,28 @@ public:
 	
 	void insert(string key,T value)
 	{
-		
+		int index = hashFn(key);
+		Node<T> *n = new Node<T>(key,value);
+		//insert at head of table index
+		n->next = table[index];
+		table[index]= n;
+		current_size++;
+	}
+	void printTable()
+	{
+		for (int i = 0; i < table_size; i++)
+		{
+			cout<<"Bucket "<<i<<" -> ";
+			Node<T>* temp = table[i];
+			while(temp !=NULL)
+			{
+				cout<<temp->key<<" ";
+				temp = temp->next;
+
+			}
+			cout<<endl;
+		}
+
 	}
 
 };
@@ -56,6 +90,12 @@ public:
 
 int main()
 {
-	
+HashTable<int> pm;
+	pm.insert("Burger",100); 
+		pm.insert("Pizza",120);
+				pm.insert("Momos",120);
+		pm.insert("Noodles",20);
+		
+		pm.printTable();
 	return 0;
 }
